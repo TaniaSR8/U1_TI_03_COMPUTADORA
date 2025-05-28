67a94f8d1f0230cb88b67745d88b4d2ff294f8ac
Tania <20243ds011@utez.edu.mx>
2025-05-28 08:47:46 -0600

    Culminación del codigo

---
+++ Main.java/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+        public class Main {
+            public static void main(String[] args) {
+                Computadora computadora = new Computadora();
+                computadora.encender();
+                computadora.encender();
+
+                computadora.setModelo("LENOVO");
+
+                System.out.println("Marca:" +computadora.getMarca());
+                System.out.println("Modelo:" +computadora.getModelo());
+
+                computadora.apagar();
+            }
+        }
+
+
+
+
+
+
